27db18e789ab14c3c0a71c79378f866aa06ab74b
Sammers21 <titantins@gmail.com>
2017-07-15 18:56:45 +0300

    delete tests + make mqtt server main verticle

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,14 +1,41 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.mqtt.MqttServer;
 
 public class MainVerticle extends AbstractVerticle {
 
-  @Override
   public void start() {
-    vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
-        .listen(8080);
-  }
+    MqttServer mqttServer = MqttServer.create(vertx);
+    mqttServer.endpointHandler(endpoint -> {
 
+      // shows main connect info
+      System.out.println("MQTT client [" + endpoint.clientIdentifier() + "] request to connect, clean session = " + endpoint.isCleanSession());
+
+      if (endpoint.auth() != null) {
+        System.out.println("[username = " + endpoint.auth().userName() + ", password = " + endpoint.auth().password() + "]");
+      }
+      if (endpoint.will() != null) {
+        System.out.println("[will topic = " + endpoint.will().willTopic() + " msg = " + endpoint.will().willMessage() +
+          " QoS = " + endpoint.will().willQos() + " isRetain = " + endpoint.will().isWillRetain() + "]");
+      }
+
+      System.out.println("[keep alive timeout = " + endpoint.keepAliveTimeSeconds() + "]");
+
+      // accept connection from the remote client
+      endpoint.accept(false);
+
+    })
+      .listen(ar -> {
+
+        if (ar.succeeded()) {
+
+          System.out.println("MQTT server is listening on port " + ar.result().actualPort());
+        } else {
+
+          System.out.println("Error on starting the server");
+          ar.cause().printStackTrace();
+        }
+      });
+  }
 }
